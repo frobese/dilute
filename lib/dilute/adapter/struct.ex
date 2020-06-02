658fa2f60ce675eb6ebb1082f44089fbbaa53219
elridion <hans@goedeke.org>
2020-06-02 10:50:49 +0200

    Fix many types in struct adapter

--- lib/dilute/adapter/struct.ex
+++ lib/dilute/adapter/struct.ex
@@ -12,23 +12,23 @@ defmodule Dilute.Adapter.Struct do
   @impl Dilute.Adapter
   def fields(module) do
     for {field, type} <- module.types() do
-      {card, mapped_type} = map_type(type)
+      {card, mapped_type, type} = map_type(type)
 
       {field, card, mapped_type, type}
     end
   end
 
   def map_type([type]) when not is_list(type) do
-    {_card, type} = map_type(type)
+    {_card, mapped_type, type} = map_type(type)
 
-    {:many, type}
+    {:many, mapped_type, type}
   end
 
   def map_type(type) do
     if applicable?(type) do
-      {:one, :"$module"}
+      {:one, :"$module", type}
     else
-      {:one, Dilute.Adapter.Ecto.map_type(type)}
+      {:one, Dilute.Adapter.Ecto.map_type(type), type}
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Dilute.MixProject do
   use Mix.Project
 
-  @version "2.0.0-rc.3"
+  @version "2.0.0-rc.4"
   def project do
     [
       app: :dilute,
